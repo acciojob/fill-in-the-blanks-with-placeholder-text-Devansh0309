e36cd5611f7edd7ee62b58ffec9ebd54a3493e34
Devansh0309 <93301328+Devansh0309@users.noreply.github.com>
2022-11-29 09:58:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+import React from 'react'
+
+function script() {
+  return (
+    <div>
+        <p>Accio ipsum dolor sit amet, join course, solve assignments, get placed above 5 lpa and live life king size.</p>
+    </div>
+  )
+}
+export default script
